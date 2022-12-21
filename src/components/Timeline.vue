f2c68d5194d5748bf036dde69289aadd79d9c618
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-21 07:48:17 +0100

    5. Vue Single File Components

---
+++ src/components/Timeline.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs">
+            <a>Today</a>
+            <a>This Week</a>
+            <a>This Month</a>
+        </span>
+    </nav>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
