966d0cf3b18247c3ca2953a6fdf3207e205a0c3a
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-21 21:03:54 +0100

    12. DateTimes with Luxon

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,7 +3,7 @@
     <nav class="is-primary panel">
         <span class="panel-tabs">
             <a 
-                v-for="period in periods"
+                v-for="period of periods"
                 :key="period"
                 :class="{'is-active': period === selectedPeriod}"
                 @click="selectPeriod(period)"
@@ -11,11 +11,19 @@
                 {{ period }}
             </a>
         </span>
+        <a v-for="post of posts" :key="post.id" class="panel-block"
+        >
+            <a>{post.title}</a>
+            <div>{post.created.toFormat("d MMM")}</div>
+        </a>
     </nav>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue"
+import {DateTime} from "luxon"
+import {Post, today, thisWeek, thisMonth} from "../posts"
+
 const periods = [
     "Today",
     "This Week",
@@ -28,7 +36,18 @@ const selectedPeriod = ref<Period>('Today')
 function selectPeriod(period: Period) {
     selectedPeriod.value = period
 }
-</script>
 
+const posts = [
+    today,
+    thisWeek,
+    thisMonth
+].map(post => {
+    return {
+        ...posts,
+        created: DateTime.fromISO(post.created)
+    }
+})
+</script>
+  
 <style scoped>
 </style>
