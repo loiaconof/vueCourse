68740ed000ffefa2c340e6c3a1b2ee6bf67a643a
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-25 11:10:10 +0100

    24. New Post Route

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="navbar">
+        <div class="navbar-end">
+            <div class="buttons">
+                <RouterLink to="/post/new" class="button">New Post</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
