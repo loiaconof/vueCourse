7d5aff081c733615fff0e979d4a733bacee3c4e3
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-25 08:18:29 +0100

    18. Extracting Complexity

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -4,14 +4,14 @@
             <a 
                 v-for="period of periods"
                 :key="period"
-                :class="{'is-active': period === selectedPeriod}"
-                @click="selectPeriod(period)"
+                :class="{'is-active': period === postsStore.selectedPeriod}"
+                @click="postsStore.setSelectedPeriod(period)"
             >
                 {{ period }}
             </a>
         </span>
         <TimelineItem
-            v-for="post of posts" 
+            v-for="post of postsStore.filteredPosts" 
             :key="post.id"
             :post="post"        
         />
@@ -19,47 +19,11 @@
 </template>
 
 <script setup lang="ts">
-import {ref, computed} from "vue"
-import {DateTime} from "luxon"
-import {TimeLinePost, today, thisWeek, thisMonth} from "../posts"
 import TimelineItem from "./TimelineItem.vue";
 import {usePosts} from "../stores/posts"
+import {periods} from "../constants"
 
 const postsStore = usePosts()
-
-const periods = ["Today", "This Week", "This Month"] as const
-type Period = typeof periods[number]
-
-const selectedPeriod = ref<Period>('Today')
-
-function selectPeriod(period: Period) {
-    selectedPeriod.value = period
-}
-
-const posts = computed<TimeLinePost[]>(() => {
-    //todo try with reduce
-    return postsStore.ids
-        .map(id => {
-            const post = postsStore.all.get(id)
-            if(!post) {
-                throw Error(`Post with id of ${id} was expected but not found`)
-            }
-            return {
-                ...post,
-                created: DateTime.fromISO(post.created)
-            }
-        }).filter(post => {
-            switch(selectedPeriod.value) {
-                case "Today":
-                    return post.created >= DateTime.now().minus({day: 1}) 
-                case "This Week":
-                    return post.created >= DateTime.now().minus({week: 1}) 
-                case "This Month":
-                default:
-                    return post
-            }
-        })
-})
 </script>
   
 <style scoped>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,22 +1,57 @@
 import {defineStore} from "pinia"
-import {Post, today, thisWeek, thisMonth} from "../posts"
+import {DateTime} from "luxon"
+import {TimeLinePost, Post, today, thisWeek, thisMonth} from "../posts"
+import {Period} from "../constants"
 
 interface PostsState {
     ids: string[] // ["1", "2", ...]
     all: Map<string, Post>
+    selectedPeriod: Period
 }
 
 export const usePosts = defineStore("posts", {
+    // reactive
     state: (): PostsState => ({
         ids: [today.id, thisWeek.id, thisMonth.id],
         all: new Map([
             [today.id, today],
             [thisWeek.id, thisWeek],
             [thisMonth.id, thisMonth],
-        ])
+        ]),
+        selectedPeriod: "Today"
     }),
 
+    // methods
     actions: {
-        // ..
+        setSelectedPeriod(period: Period) {
+            this.selectedPeriod = period
+        }
+    },
+
+    // similar to computed properties
+    getters: {
+        filteredPosts: (state): TimeLinePost[] => {
+            return state.ids
+                .map(id => {
+                    const post = state.all.get(id)
+                    if(!post) {
+                        throw Error(`Post with id of ${id} was expected but not found`)
+                    }
+                    return {
+                        ...post,
+                        created: DateTime.fromISO(post.created)
+                    }
+                }).filter(post => {
+                    switch(state.selectedPeriod) {
+                        case "Today":
+                            return post.created >= DateTime.now().minus({day: 1}) 
+                        case "This Week":
+                            return post.created >= DateTime.now().minus({week: 1}) 
+                        case "This Month":
+                        default:
+                            return post
+                    }
+                })
+        }
     }
 })
