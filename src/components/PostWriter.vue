58038f43651d4b7a46f2deaed7c9b1cad466dda5
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-25 19:08:11 +0100

    26. Two way binding with v-model

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -3,16 +3,19 @@
         <div class="column">
             <div class="field">
                 <div class="label">Post title</div>
-                <input type="text" class="input">
+                <input type="text" class="input" v-model="title">
+                {{ title }}
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { TimeLinePost } from '../posts';
 
 const props = defineProps<{
     post: TimeLinePost
 }>()
+const title = ref(props.post.title)
 </script>
