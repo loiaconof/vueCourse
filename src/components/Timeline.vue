3f2732301c157224eb369ce2ded3c3b3af258f12
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-25 10:11:09 +0100

    20. Fetching Data with Fetch

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -24,6 +24,7 @@ import {usePosts} from "../stores/posts"
 import {periods} from "../constants"
 
 const postsStore = usePosts()
+postsStore.fetchPosts()
 </script>
   
 <style scoped>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -9,15 +9,15 @@ interface PostsState {
     selectedPeriod: Period
 }
 
+function delay() {
+    return new Promise<void>(res => setTimeout(res, 1500))
+}
+
 export const usePosts = defineStore("posts", {
     // reactive
     state: (): PostsState => ({
-        ids: [today.id, thisWeek.id, thisMonth.id],
-        all: new Map([
-            [today.id, today],
-            [thisWeek.id, thisWeek],
-            [thisMonth.id, thisMonth],
-        ]),
+        ids: [],
+        all: new Map(),
         selectedPeriod: "Today"
     }),
 
@@ -25,6 +25,22 @@ export const usePosts = defineStore("posts", {
     actions: {
         setSelectedPeriod(period: Period) {
             this.selectedPeriod = period
+        },
+
+        async fetchPosts() {
+            const res = await window.fetch("http://localhost:8000/posts")
+            const data = (await res.json()) as Post[]
+            await delay()
+
+            let ids: string[] = []
+            let all = new Map<string, Post>()
+            for(const post of data) {
+                ids.push(post.id)
+                all.set(post.id, post)
+            }
+            
+            this.ids = ids
+            this.all = all
         }
     },
 
