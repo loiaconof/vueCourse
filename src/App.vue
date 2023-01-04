f7a233ca4680dd8016a4e1274ad94f5e95203a7e
loiaconof <92976965+loiaconof@users.noreply.github.com>
2023-01-04 09:02:06 +0100

    37. Creating the useModal Composable

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script lang="ts" setup>
 import Navbar from './components/Navbar.vue';
+import {useModal} from './composables/modal'
+import {computed} from 'vue'
+
+const modal = useModal()
+const modalStyle = computed(() => {
+    return {
+      display: modal.show.value ? 'block' : 'none'
+    }
+})
 </script>
 
 <template>
   <div 
     class="modal"
-    style="display: block; color:white"
+    style="color:white;"
+    :style="modalStyle"
   >
     <div class="modal-background">
       <div class="modal-content">
         <div id="modal"></div>
       </div>
     </div>
+    <button class="modal-close is-large" @click="modal.hideModal"></button>
   </div>
   <div class="section">
     <div class="container">
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
     <div class="navbar">
         <div class="navbar-end">
             <div class="buttons">
+                <button class="button" @click="modal.showModal">Sign Up</button>
                 <RouterLink to="/post/new" class="button">New Post</RouterLink>
             </div>
         </div>
@@ -11,3 +12,9 @@
         Content to be rendered.
     </Teleport>
 </template>
+
+<script lang="ts" setup>
+import {useModal} from '../composables/modal'
+
+const modal = useModal()
+</script>
