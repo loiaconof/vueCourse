0c34610dee199d62f61aa0280b281507782fba69
Francesco <francesco.loiacono@linksmt.it>
2023-02-08 08:21:13 +0100

    61. Conditionally Editing Post

--- src/views/ShowPost.vue
+++ src/views/ShowPost.vue
@@ -2,7 +2,10 @@
   <div class="columns">
     <div class="column"></div>
     <div class="column is-two-thirds">
-      <RouterLink :to="`/posts/${post.id}/edit`" class="is-link button is-rounded">
+      <RouterLink
+          v-if="canEdit"
+          :to="`/posts/${post.id}/edit`"
+          class="is-link button is-rounded">
         Edit Post
       </RouterLink>
       <h1>{{post.title}}</h1>
@@ -15,9 +18,12 @@
 <script lang="ts" setup>
 import {useRoute} from 'vue-router'
 import {usePosts} from "../stores/posts";
+import {useUsers} from "../stores/users";
 
 const route = useRoute()
 const postsStore = usePosts()
+const userStore = useUsers()
+
 const id = route.params.id as string
 
 const post = postsStore.all.get(id)
@@ -25,4 +31,16 @@ const post = postsStore.all.get(id)
 if(!post) {
   throw Error(`Post with id ${id} was not found`)
 }
+
+const canEdit = computed(()=> {
+  if(!userStore.currentUserId) {
+    return false
+  }
+
+  if(userStore.currentUserId !== post.authorId) {
+    return false
+  }
+
+  return true
+})
 </script>
